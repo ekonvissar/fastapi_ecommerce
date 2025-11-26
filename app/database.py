14cc41a478886313031f437727707e50bc18da0f
Eugen <ekonvissar@gmail.com>
2025-11-26 21:45:13 +0300

    Delete alembic.ini delete mirgations create async alembic, migration and set alembic

--- app/database.py
+++ app/database.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from sqlalchemy.orm import sessionmaker
 from decouple import config
 
 # Строка подключения для SQLite
