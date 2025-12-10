bd8c5f218fbb738881876291ab8ef9634467a088
Eugen <ekonvissar@gmail.com>
2025-12-10 21:15:58 +0300

    create PostgreSQL Full-Text Search

--- app/models/products.py
+++ app/models/products.py
@@ -2,15 +2,10 @@
 from decimal import Decimal
 from typing import TYPE_CHECKING
 
-from sqlalchemy import (
-    Boolean,
-    ForeignKey,
-    Integer,
-    Numeric,
-    String,
-    text,
-    Float,
-)
+from sqlalchemy import (Boolean, ForeignKey, Integer, Numeric,
+                        String, text, Float, Computed, Index,
+                        )
+from sqlalchemy.dialects.postgresql import TSVECTOR
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.database import Base
@@ -33,8 +28,24 @@ class Product(Base):
     category_id: Mapped[int] = mapped_column(ForeignKey("categories.id"), nullable=False)
     seller_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
 
+    tsv: Mapped[TSVECTOR] = mapped_column(
+        TSVECTOR,
+        Computed(
+            """
+            setweight(to_tsvector('english', coalesce(name, '')), 'A')
+            ||
+            setweight(to_tsvector('english', coalesce(description, '')), 'B')
+            """,
+            persisted=True,
+        ),
+        nullable=False
+    )
+
+
     category: Mapped["Category"] = relationship(back_populates="products")
     seller = relationship("User", back_populates="products")
     review = relationship("Review", back_populates="product")
 
-
+    __table_args__ = (
+        Index("ix_products_tsv_gin", "tsv", postgresql_using="gin"),
+    )
