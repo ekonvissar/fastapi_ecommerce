ce30314e9815b2e24516e41a741b62d438db2acb
Eugen <ekonvissar@gmail.com>
2025-12-23 07:52:45 +0300

    Create Simple Chat via WebSocket

--- app/main.py
+++ app/main.py
@@ -1,16 +1,18 @@
-# from celery.schedules import crontab
-from fastapi import FastAPI, Request
+from typing import List
+
+from fastapi import FastAPI, Request, WebSocket, WebSocketDisconnect
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.middleware.trustedhost import TrustedHostMiddleware
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 from starlette.middleware.gzip import GZipMiddleware
 from starlette.middleware.httpsredirect import HTTPSRedirectMiddleware
 from loguru import logger
-from app.logging import log_middleware
 from celery import Celery
 
 from app.task import call_background_task
-
+from app.logging import log_middleware
 
 from app.middleware import (log_request_middleware,
                             error_handler,
@@ -23,6 +25,11 @@ app = FastAPI(
     title='FastAPI интеренет-магазин',
     version='0.1.0',
 )
+app_v1 = FastAPI(
+    title='WebSocket API',
+)
+
+templates = Jinja2Templates(directory="app/templates")
 
 celery = Celery(
     __name__,
@@ -43,7 +50,7 @@ app.add_middleware(TrustedHostMiddleware, allowed_hosts=["localhost", "127.0.0.1
 app.add_middleware(HTTPSRedirectMiddleware)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://localhost:8000", "null"],
+    allow_origins=["https://localhost:8000", "https://127.0.0.1:8000", "null"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -88,3 +95,36 @@ celery.conf.beat_schedule = {
         'args': ('Test text message',)
     }
 }
+class ConnectionManager:
+    def __init__(self):
+        self.connections: List[WebSocket] = []
+        print("Creating a list to active connections", self.connections)
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.connections.append(websocket)
+        print("New Active connections are ", self.connections)
+
+    async def broadcast(self, data: str):
+        for connection in self.connections:
+            await connection.send_text(data)
+            print("In broadcast: sent msg to ", connection)
+
+manager = ConnectionManager()
+
+@app_v1.get("/", response_class=HTMLResponse)
+def read_index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app_v1.websocket("/ws/{client_id}")
+async def websocket_endpoint(websocket: WebSocket, client_id: int):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await manager.broadcast(f'Client {client_id}: {data}')
+    except WebSocketDisconnect as e:
+        manager.connections.remove(websocket)
+        print(f'Connection closed {e.code}')
+
+app.mount("/v1", app_v1)
